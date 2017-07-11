7af2097b2e6b22fb2ce5d2492dc3a980b1ea5617
Casey Platt <kse0824@gmail.com>
2017-07-11 16:02:35 -0500

    Correct Angular code

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -4,7 +4,7 @@
   angular.module("app").controller("stuffCtrl", function($scope) {
   	$scope.myMessage = "No matter where you go...";
   	$scope.myName = "Casey";
-  	$scope.sum = 234 + 5476;
+  	$scope.Sum = 234 + 5476;
   });
  
 }());
