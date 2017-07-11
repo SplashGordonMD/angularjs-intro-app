de3d6e08f939bb15eb94a737e205a03bf1ef1d2e
Casey Platt <kse0824@gmail.com>
2017-07-11 16:43:45 -0500

    Angular exercize

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -5,8 +5,8 @@
   	$scope.task1 = "There you are...";
   	$scope.task2 = "KSE";
   	$scope.task3 = 6734 * 892;
-
   	$scope.review = "Angular Exercise";
+  	$scope.arrays = ["Anybody", "Out", "There"];
   });
  
 }());
