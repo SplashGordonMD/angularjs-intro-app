f9817e4e44f296f094322e38d9ff4ba131a76d1a
Casey Platt <kse0824@gmail.com>
2017-07-12 09:01:36 -0500

    ng model controller click

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -2,11 +2,16 @@
   "use strict";
 
   angular.module("app").controller("taskCtrl", function($scope) {
-  	$scope.task1 = "There you are...";
-  	$scope.task2 = "KSE";
-  	$scope.task3 = 6734 * 892;
+  	$scope.task1 = "Hey you, can you help them to berry the stone...";
+  	$scope.task2 = "Casey's review of Peter Jangs intro to Angular";
+  	$scope.task3 = 144 / 12;
+  	$scope.loops = ["Just", "one", "more", "morning" ];
   	$scope.review = "Angular Exercise";
   	$scope.arrays = ["Anybody", "Out", "There"];
+
+  	$scope.addTask = function() {
+ 	  console.log("Me an my RC");
+  	}
   });
  
 }());
