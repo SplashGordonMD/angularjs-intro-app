47d21d673bbeecac86a029b7972df7c5f45f0c16
Casey Platt <kse0824@gmail.com>
2017-07-12 12:15:25 -0500

    single attribute data

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -1,23 +1,20 @@
+/* global angular */
+
 (function() {
-  "use strict";
+  angular.module("app").controller("tasksCtrl", function($scope) {
+  	$scope.tasks = [ 
+  	  "No Body Home",
+  	  "Confortable Numb", 
+  	  "The Trial", 
+  ];
 
-  angular.module("app").controller("stuffCtrl", function($scope) {
-  	$scope.reviews = [ 
-  		{task: "No Body Home", side: 2, reviewer: "Chewwy"},
-  		{task: "Confortable Numb", side: 3, reviewer: "Hagan"},
-  	    {task: "The Trial", side: 4, reviewer: "Ox"},
-  	    ];
-
-  	$scope.addReview = function(inputTask) {
-  		$scope.reviews.push(inputTask);
-  	$scope.addReview = function(inputTask, inputSide, inputName) {
-  		var newReview = {task: inputTask, side: inputSide, name: inputName};
- 	  $scope.reviewss.push(inputReview);
-  	};
-
-    $scope.isPositive = function(inputReviewTask) {
-    	return (inputReviewTask.indexOf("bad") === -1);
-    $scope.isPositive = function(inputReview) {
-    	return  (inputReview.Task.indexOf("bad") === -1);
-  };
- });
+  	$scope.addTask = function(inputTask) {
+  	  if (inputTask !=='')	{
+  		$scope.tasks.push(inputTask);
+  		$scope.newTask = '';
+  	 } else {
+  	 	console.log(inputTask);
+  	 }	
+    };
+  });
+})(); 
