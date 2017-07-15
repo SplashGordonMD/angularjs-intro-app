a62b43078b5317b171560762b798ed95459a4d1f
Casey Platt <kse0824@gmail.com>
2017-07-15 10:42:44 -0500

    start upgrading angular

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -1,24 +1,15 @@
 /* global angular */
 
 (function() {
-  angular.module("app").controller("tasksCtrl", function($scope) {
-  	$scope.tasks = [ 
-  	  "No Body Home",
-  	  "Confortable Numb", 
-  	  "The Trial", 
-  ];
+  angular.module("app").controller("stuffCtrl", function($scope) {
+  	$scope.myMessage = "Good Morning July 12.2017!!!"; 
+  	$scope.myName = "Casey Platt";
+  	$scope.sum = 432 + 987654;
+  	$scope.reviews = ["Good", "Bad", "Indeferent"];  
 
-  	$scope.addTask = function(inputTask) {
-  	  if (inputTask !=='')	{
-  		$scope.tasks.push(inputTask);
-  		$scope.newTask = '';
-  	 } 
-    };
-    $scope.completeTask = function(inputTask) {
-     var index = $scope.tasks.indexOf(inputTask);
-     if (index > -1) {
-       $scope.tasks.splice(index, 1);	
-    };
-   };
-  }); 
-})(); 
+  	$scope.addReview = function(inputText) {
+  		$scope.reviews.push(inputText);
+  		
+  	 }; 
+    });
+   
